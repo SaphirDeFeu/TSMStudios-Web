9371e0efaa703f9c867c072dfb469e3b89c7b9e6
SaphirDeFeu <123899399+SaphirDeFeu@users.noreply.github.com>
2023-10-30 15:53:51 +0100

    Custom Error Page
    - That's it

---
+++ client/error.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Whoops!</h1>
+    <h2>{{ error.statusCode }} - {{ error.statusMessage }}</h2>
+    <p>Additional information: <br/>{{ error.message }}<br/>{{ error.description }}</p>
+    <button @click="back">Back to home?</button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  error: Object
+})
+
+const back = () => clearError({ redirect: '/' });
+</script>
+
+<style scoped lang="scss">
+</style>
